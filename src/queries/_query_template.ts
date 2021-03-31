5a9413c4281bedfb5f0f562d82b0edebe4fef974
fsantos22 <74519608+fsantos22@users.noreply.github.com>
2021-03-31 14:16:11 -0300

    Delete _query_template.ts

--- src/queries/_query_template.ts
+++
@@ -1,7 +0,0 @@
-import connection from '../connection';
-
-export default async function query_template(): Promise<any> {
-  const result = await connection.raw(`mySQL function`);
-
-  return result[0];
-}
